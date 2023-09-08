dc38ac233991a5757e9fe262968290aab84aebaf
Harlihm <107759287+Harlihm@users.noreply.github.com>
2023-09-08 07:54:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,4 +13,4 @@ date.append(`${now.toLocaleDateString(undefined,options)}`);
 time.append(`${now.getUTCHours()}:${now.getMinutes()}:${now.getSeconds()}:${now.getMilliseconds()}`);
 
 }
-setInterval(updateDateTime,1000);
+setInterval(updateDateTime,6000);
